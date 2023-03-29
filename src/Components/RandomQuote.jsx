0331c1e9df49c8ab372389753d08b239acff58f9
ialcaine <irenealcainealvarez@gmail.com>
2023-03-29 07:40:07 +0000

    Rename components

--- src/Components/RandomQuote.jsx
+++ src/Components/RandomQuote.jsx
@@ -1,36 +1,39 @@
 import React, { useState } from "react";
-import RandomQuotes from "../data/RandomQuotes.js";
+import Quotes from "../data/Quotes.js";
 
-const Quote = () => {
-
-  const [fraseAleatoria, setFraseAleatoria] = useState('');
+const RandomQuote = () => {
+  const [fraseAleatoria, setFraseAleatoria] = useState("");
   const [mostrarExplicacion, setMostrarExplicacion] = useState(false);
 
-
   const generarFraseAleatoria = () => {
-    const indiceAleatorio = Math.floor(Math.random() * RandomQuotes.length);
-    const frase = RandomQuotes[indiceAleatorio];
+    const indiceAleatorio = Math.floor(Math.random() * Quotes.length);
+    const frase = Quotes[indiceAleatorio];
     setFraseAleatoria(frase);
     setMostrarExplicacion(false);
-
   };
   const toggleMostrarExplicacion = () => {
     setMostrarExplicacion(!mostrarExplicacion);
   };
 
   return (
-
     <div className="bg-blue-100 flex flex-col items-center justify-center">
-
       {fraseAleatoria && (
         <div className="mb-8 px-4 py-2 bg-blue-900 text-white rounded-lg w-10/12 md:w-9/12 text-center">
-          <p className={`text-3xl md:text-6xl uppercase font-bold font-oswald tracking-tighter after:content-['"'] before:content-['"']`}>{fraseAleatoria.randomQuote}</p>
-          <p className="text-lg mt-4 text-right italic before:content-['-_']">{fraseAleatoria.author}</p>
+          <p
+            className={`text-3xl md:text-6xl uppercase font-bold font-oswald tracking-tighter after:content-['"'] before:content-['"']`}
+          >
+            {fraseAleatoria.randomQuote}
+          </p>
+          <p className="text-lg mt-4 text-right italic before:content-['-_']">
+            {fraseAleatoria.author}
+          </p>
           <button
             className="mt-4 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
             onClick={toggleMostrarExplicacion}
           >
-            {mostrarExplicacion ? 'Ocultar explicación' : '¿Por qué me inspira?'}
+            {mostrarExplicacion
+              ? "Ocultar explicación"
+              : "¿Por qué me inspira?"}
           </button>
           {mostrarExplicacion && (
             <p className="mt-4 mb-8 w-10/12 px-4 py-2 mx-auto text-lg border-2 border-blue-300 rounded-xl">
@@ -38,19 +41,16 @@ const Quote = () => {
             </p>
           )}
         </div>
-      )
-      }
+      )}
 
       <button
         className="bg-blue-700 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded"
         onClick={generarFraseAleatoria}
       >
-
         {!fraseAleatoria ? "Generar frase aleatoria" : "Generar otra frase"}
       </button>
-    </div >
-
+    </div>
   );
 };
 
-export default Quote;
+export default RandomQuote;
---
+++ src/data/Quotes.js
@@ -0,0 +1,22 @@
+const Quotes = [
+  {
+    quote: "A la mierda",
+    author: "Fernando Fernán Gomez",
+    inspiration:
+      "Porque a veces es necesario mandarlo todo a la mierda, que se le va a hacer",
+  },
+  {
+    quote:
+      "Hemos nacido demasiado tarde para exlorar el mundo, demasiado pronto para explorar el espacio y demasiado cuerdos para explorar el fondo submarino",
+    author: "Tri-line",
+    inspiration: "Porque nunca estamos donde debemos.",
+  },
+  {
+    quote: "No entiendo nada",
+    author: "Michael Scott",
+    inspiration:
+      "Porque a veces es necesario mandarlo todo a la mierda, que se le va a hacer",
+  },
+];
+
+export default Quotes;
