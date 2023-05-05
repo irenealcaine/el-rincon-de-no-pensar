206d496026b90b0d1656fa4596ce0c648be1aa50
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-05 05:43:10 +0000

    Update Error.jsx

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Components/Header";
+import { FaTools } from "react-icons/fa";
 
 const Error = () => {
   return (
-    <div className="bg-blue-100">
+    <div className="bg-blue-100 h-full">
       <Header title={"Error"} />
-      UPS
+      <FaTools className="w-5/12 h-60 mx-auto" />
+      <p className="text-center p-8">Working on...</p>
     </div>
   );
 };
