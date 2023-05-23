f2f77585824f8680d1dc48073133bf3969c04692
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-23 11:24:11 +0000

    Update BlogPostDetail.jsx and Posts.js

--- src/Pages/BlogPostDetail.jsx
+++ src/Pages/BlogPostDetail.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import Header from "../Components/Header";
+import Subtitle from "../Components/Subtitle";
 import Posts from "../data/Posts";
 
 const BlogPostDetail = () => {
@@ -7,8 +9,10 @@ const BlogPostDetail = () => {
   const post = Posts.find((post) => post.id === parseInt(id));
   console.log(id, Posts, post);
   return (
-    <div className="bg-blue-100">
-      <h2>{post.title}</h2>
+    <div className="bg-blue-100 min-h-screen">
+      <Header title={post.title} />
+      <img src={post.imageUrl} alt={post.title} />
+      <Subtitle subtitle={post.excerpt} />
     </div>
   );
 };
