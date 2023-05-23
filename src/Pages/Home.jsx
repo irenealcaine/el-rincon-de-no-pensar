0b22efbf0a359902470b8b90035e44d9a4313a96
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-23 09:37:45 +0000

    Update 5 files

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,18 +8,21 @@ const Home = () => {
     <div className="bg-blue-100 min-h-screen">
       <Header title={"El rincón de no pensar"} />
       <div className="flex flex-col md:flex-row gap-8 justify-center">
-      {
-        menuItems.map((item)=>{
-          return(
-            <Link
-            to={item.to}
-            className="flex flex-col items-center text-blue-900">
-              <p className="p-4 border-2 border-blue-900 rounded-full">{item.bigIcon}</p>
-              <p className="">{item.title}</p>
-            </Link>
-          )
-        })
-      }
+        {menuItems.map((item) => {
+          return (
+            <div className="">
+              <Link
+                to={item.to}
+                className=" flex flex-col items-center text-blue-900"
+              >
+                <p className="p-4 border-2 border-blue-900 rounded-full hover:bg-blue-500 hover:text-white transition duration-300">
+                  {item.bigIcon}
+                </p>
+                <p className="">{item.title}</p>
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
