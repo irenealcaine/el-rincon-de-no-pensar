990c38b64348d77276f10ee0abb3c3a7a531421f
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-29 11:03:32 +0000

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,18 @@
-function Card({item, id, handleClick}){
-
-    return (
-        <div className={`border-2 border-blue-700 p-4 rounded-xl w-32 h-32 ${item.stat === "correct" ? "bg-green-300" : ""} ${item.stat === "wrong" ? "bg-red-300" : ""}`} onClick={() => handleClick(id)}>
-            <img src={item.img} alt="/" className={`${item.stat ? "w-full h-full" : "h-0 w-0" } `}/>
-        </div>
-    )
+function Card({ item, id, handleClick }) {
+  return (
+    <div
+      className={`border-2 border-blue-700 p-4 rounded-xl w-5/12 max-w-[180px] md:w-3/12 lg:w-2/12 aspect-square ${
+        item.stat === "correct" ? "bg-green-300" : ""
+      } ${item.stat === "wrong" ? "bg-red-300" : ""}`}
+      onClick={() => handleClick(id)}
+    >
+      <img
+        src={item.img}
+        alt="/"
+        className={`${item.stat ? "w-full h-full" : "h-0 w-0"} `}
+      />
+    </div>
+  );
 }
 
-export default Card
+export default Card;
