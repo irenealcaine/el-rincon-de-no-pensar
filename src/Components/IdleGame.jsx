78c3e91eb35562ffbe30130d316abdc70b8f2694
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-06-05 07:27:10 +0000

    Update IdleGame.jsx

--- src/Components/IdleGame.jsx
+++ src/Components/IdleGame.jsx
@@ -5,92 +5,97 @@ const IdleGame = () => {
   const [score, setScore] = useState(0);
   const [bonusButtonDisabled, setBonusButtonDisabled] = useState(true);
   const [baseButtonDisabled, setBaseButtonDisabled] = useState(true);
-  const [baseLvl, setBaseLvl] = useState(1);
-  const [baseCost, setBaseCost] = useState(5);
-  const [bonus1Lvl, setBonus1Lvl] = useState(0);
-  const [bonus1Cost, setBonus1Cost] = useState(5);
+  const [base, setBase] = useState({ level: 0, cost: 100 });
+  const [bonus1, setBonus1] = useState({ level: 0, cost: 5 });
+  const E = 2.71828;
 
   const handleClick = () => {
-    setScore(score + baseLvl * 1.1);
+    setScore((prevScore) => prevScore + Math.pow(2, base.level));
   };
 
   const handleReset = () => {
     setScore(0);
-    setBonus1Lvl(0);
-    setBonus1Cost(5);
-    setBaseLvl(1);
-    setBaseCost(5);
-    console.log("bonus 1 lvl: " + bonus1Lvl);
+    setBonus1({ level: 0, cost: 5 });
+    setBase({ level: 1, cost: 5 });
   };
 
   useEffect(() => {
     const bonusInterval = setInterval(() => {
-      setScore((prevScore) => prevScore + bonus1Lvl * 0.01);
+      setScore((prevScore) => prevScore + bonus1.level * 0.1);
     }, 1000);
 
     return () => {
       clearInterval(bonusInterval);
     };
-  }, [bonus1Lvl]);
+  }, [bonus1.level]);
 
   useEffect(() => {
-    if (score >= bonus1Cost) {
-      setBonusButtonDisabled(false);
-    } else {
-      setBonusButtonDisabled(true);
-    }
-  }, [score, bonus1Cost]);
+    setBonusButtonDisabled(score < bonus1.cost);
+  }, [score, bonus1.cost]);
 
   useEffect(() => {
-    if (score >= baseCost) {
-      setBaseButtonDisabled(false);
-    } else {
-      setBaseButtonDisabled(true);
-    }
-  }, [score, baseCost]);
+    setBaseButtonDisabled(score < base.cost);
+  }, [score, base.cost]);
 
   const handleBaseClick = () => {
-    setBaseLvl(baseLvl + 1);
-    setBaseCost(baseCost * 1.5);
-    console.log("bonus 1 lvl: " + bonus1Lvl);
+    setScore(score - base.cost);
+    setBase((prevBase) => ({
+      ...prevBase,
+      level: prevBase.level + 1,
+      cost: prevBase.cost ** 2,
+    }));
+    console.log(base);
   };
 
   const handleBonusClick = () => {
-    setBonus1Lvl(bonus1Lvl + 1);
-    setBonus1Cost(bonus1Cost * 1.5);
-    console.log("bonus 1 lvl: " + bonus1Lvl);
+    setScore(score - bonus1.cost);
+    setBonus1((prevBonus1) => ({
+      ...prevBonus1,
+      level: prevBonus1.level + 1,
+      cost: prevBonus1.cost ** 2,
+    }));
+    console.log(bonus1);
   };
 
   return (
     <div>
       <p className="text-4xl">Puntuación: {score.toFixed(2)} puntos</p>
-      <p>{(baseLvl * 1.1).toFixed(2)} puntos por click</p>
+      <hr />
+      <p>--------------------------------------</p>
+
+      <p>{base.level * 1.1} puntos por click</p>
       <Button
         className={"bg-green-500"}
         onClickValue={handleClick}
         value={"Click"}
       />
+      <hr />
+      <p>--------------------------------------</p>
       <p>
-        Subir nivel base (prox lvl: {((baseLvl + 1) * 1.1).toFixed(2)} puntos
+        Subir nivel bonus 1 ({bonus1.level * 0.01} puntos/segundo, prox lvl{" "}
+        {(bonus1.level + 1) * 0.01} puntos/segundo)
+      </p>
+      <Button
+        className={bonusButtonDisabled && "bg-gray-400"}
+        onClickValue={handleBonusClick}
+        value={bonus1.level + " " + bonus1.cost}
+        disabled={bonusButtonDisabled}
+      />
+      <hr />
+      <p>--------------------------------------</p>
+      <p>
+        Subir nivel base (prox lvl: {(parseInt(base.level) + 1) * 1.1} puntos
         por click)
       </p>
       <Button
         className={baseButtonDisabled && "bg-gray-400"}
         onClickValue={handleBaseClick}
-        value={baseLvl + " " + baseCost.toFixed(2)}
+        value={base.level + " " + base.cost}
         disabled={baseButtonDisabled}
       />
-      <p>
-        Subir nivel bonus 1 ({bonus1Lvl * 0.01} puntos/segundo, prox lvl{" "}
-        {(bonus1Lvl + 1) * 0.01} puntos/segundo)
-      </p>
-      <Button
-        className={bonusButtonDisabled && "bg-gray-400"}
-        onClickValue={handleBonusClick}
-        value={bonus1Lvl + " " + bonus1Cost.toFixed(2)}
-        disabled={bonusButtonDisabled}
-      />
-      <p>Resetear</p>
+      <hr />
+      <p>--------------------------------------</p>
+
       <Button
         className={"bg-red-700"}
         onClickValue={handleReset}
