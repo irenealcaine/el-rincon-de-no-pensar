6a30379d69391f1b6918697eb79965aef9613bd2
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-06-05 16:31:59 +0000

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,18 +3,39 @@ import Header from "../Components/Header";
 import Subtitle from "../Components/Subtitle";
 import menuItems from "../data/MenuItems";
 import ListItems from "../Components/ListItems";
+import { GoMail } from "react-icons/go";
 
 const Home = () => {
-  const introduction1 = `Bienvenidos a 'El Rincón de no pensar', un espacio virtual donde se encuentran proyectos pequeños pero llenos de encanto. Aquí, en esta acogedora web, les presento una colección de creaciones modestas que, aunque demasiado pequeñas para tener su propio sitio web, merecen ser apreciadas y compartidas.`;
-  const introduction2 = `En este rincón digital, encontrarás una variedad de proyectos, componentes sueltos y juegos, todos ellos desarrollados con amor utilizando las tecnologías React y Tailwind CSS. ¡Espero que disfrutes de tu visita y encuentres inspiración en estos proyectos nacidos de la pasión por el desarrollo web!`;
+  const introduction1 = `Bienvenidos a 'El Rincón de no pensar', una web donde se encuentran proyectos pequeños pero llenos de encanto, todos ellos hecos con React y Tailwind. Aquí, presento una colección de creaciones modestas que, aunque demasiado pequeñas para tener su propio sitio web, merecen ser apreciadas y compartidas.`;
 
   return (
     <div className="bg-blue-100 min-h-screen">
       <Header title={"El rincón de no pensar"} />
       <Subtitle subtitle={introduction1} />
       <ListItems mapItems={menuItems} />
-      <Subtitle subtitle={introduction2} />
-      <p>Contacto:</p>
+      <p className="p-8">
+        Todo el código está disponible en{" "}
+        <a
+          href="https://github.com/irenealcaine/el-rincon-de-no-pensar"
+          target="_blank"
+          rel="noreferrer"
+          className="underline"
+        >
+          Github
+        </a>
+        , siéntete libre de curiosear
+      </p>
+      <p>
+        Contacto:{" "}
+        <a
+          href="mailto:irenealcainealvarez@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+          className="underline"
+        >
+          <GoMail className="inline-block" /> irenealcainealvarez@gmail.com
+        </a>
+      </p>
     </div>
   );
 };
