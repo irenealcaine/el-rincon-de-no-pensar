1a7b5bb3294c1e3996bc9fb389a4508d35ad705f
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-06-20 17:32:33 +0200

    update RPS.jsx

--- src/Components/RPS.jsx
+++ src/Components/RPS.jsx
@@ -35,10 +35,10 @@ const RPS = () => {
       (user === tijeras && computer === papel)
     ) {
       setWins(wins + 1);
-      return 'Ganaste';
+      return 'Has ganado';
     } else {
       setLosses(losses + 1);
-      return 'Perdiste';
+      return 'Has perdido';
     }
   };
 
@@ -50,22 +50,22 @@ const RPS = () => {
 
   return (
     <div className="flex justify-center items-center">
-      <div className="text-center">
+      <div className="text-center w-10/12">
         <div className="flex justify-center mb-4">
           {options.map((option) => (
-            <Button value={option} onClickValue={() => handleUserChoice(option)} disabled={!!userChoice} className={`mx-2 ${userChoice === option && 'outline outline-offset-2 outline-blue-700'}`} type={!!userChoice && "gray"} />
+            <Button value={option} onClickValue={() => handleUserChoice(option)} disabled={!!userChoice} className={`mx-2 !px-2 ${userChoice === option && 'outline outline-offset-2 outline-blue-700'}`} type={!!userChoice && "gray"} />
           ))}
         </div>
-        <div className='flex justify-center gap-4 mx-auto py-2 px-4 border border-blue-500 rounded'>
-          <p className="">Ganadas: {wins}</p>
-          <p className="">Perdidas: {losses}</p>
-          <p className="">Empates: {ties}</p>
+        <div className='flex justify-center gap-4 mx-auto w-full md:w-7/12 py-2 px-4 border border-blue-500 rounded'>
+          <p className="">Ganadas: <span className={`text-xl ${wins > losses && wins > ties ? "font-bold text-green-700" : ""}`}>{wins}</span></p>
+          <p className="">Perdidas: <span className={`text-xl ${losses > wins && losses > ties ? "font-bold text-red-700" : ""}`}>{losses}</span></p>
+          <p className="">Empates: <span className={`text-xl ${ties > wins && ties > losses ? "font-bold text-blue-700" : ""}`}>{ties}</span></p>
         </div>
         {userChoice && computerChoice && result && (
           <div>
-            <p className='mt-4 mb-2'>Tu elección: {userChoice}</p>
-            <p>Elección de la inteligencia artificial: {computerChoice}</p>
-            <h2 className="text-2xl my-4">{result}</h2>
+            <p className='mt-4 mb-2 text-blue-700'>Tu elección: <span className='ml-2'>{userChoice}</span></p>
+            <p className="text-green-700">Elección de la inteligencia artificial: <span className='ml-2'>{computerChoice}</span></p>
+            <h2 className="text-2xl my-4 font-bold">{result}</h2>
 
             <Button value={"Otra vez"} onClickValue={handleReset} />
           </div>
