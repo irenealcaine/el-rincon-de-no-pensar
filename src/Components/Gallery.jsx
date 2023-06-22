d4e6e526dafb36cb07e51db3bf465b3ea8050dbd
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-06-22 12:19:16 +0000

    Update Gallery.jsx

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,7 +1,30 @@
 import React, { useState } from "react";
 
+const Modal = ({ isOpen, onClose, imgSrc }) => {
+  return (
+    <div
+      className={`fixed inset-0 flex items-center justify-center z-10 ${
+        isOpen ? "" : "hidden"
+      }`}
+    >
+      <div className="absolute inset-0 bg-gray-900/40"></div>
+      <div className="p-8 rounded z-20 mx-auto">
+        <img alt={""} src={imgSrc} className={"mx-auto rounded-xl"} />
+        <button
+          className="mt-4 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded"
+          onClick={onClose}
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
 const Gallery = ({ photos }) => {
   const [selectedCategory, setSelectedCategory] = useState("Todas");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [imgSrc, setImgSrc] = useState("");
 
   const categories = [
     "Todas",
@@ -12,6 +35,15 @@ const Gallery = ({ photos }) => {
     selectedCategory === "Todas"
       ? photos
       : photos.filter((photo) => photo.category === selectedCategory);
+
+  const openModal = (src) => {
+    setIsModalOpen(true);
+    setImgSrc(src);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div className="container mx-auto p-4">
@@ -32,16 +64,18 @@ const Gallery = ({ photos }) => {
           ))}
         </select>
       </div>
-      <div className="columns-2 gap-1">
+      <div className="columns-2 gap-4">
         {filteredPhotos.map((photo) => (
-          <div key={photo.id} className="border p-2">
+          <div key={photo.id} className="">
             <img
               src={photo.url}
               alt={photo.title}
-              className="w-full rounded-lg shadow bg-cover"
+              onClick={() => openModal(photo.url)}
+              className="w-full rounded-lg shadow bg-cover border-2 border-transparent hover:border-blue-700 transition duration-200 mb-4 cursor-pointer"
             />
           </div>
         ))}
+        <Modal isOpen={isModalOpen} onClose={closeModal} imgSrc={imgSrc} />
       </div>
     </div>
   );
