5230c9ecc7462117b79f11e99b4be1c1cc5433e0
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-06-23 12:08:26 +0000

    Update Idle.jsx

--- src/Pages/Idle.jsx
+++ src/Pages/Idle.jsx
@@ -1,11 +1,23 @@
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import IdleGame from "../Components/IdleGame";
+import { useNavigate } from "react-router-dom";
+import Button from "../Components/Button";
 
 const Idle = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="min-h-screen bg-blue-100">
       <Header title={"Clicker"} />
+      <Button
+        type={"violet"}
+        className={"ml-8"}
+        onClickValue={() => {
+          navigate("/games");
+        }}
+        value={"Juegos"}
+      />
       <IdleGame />
       <Footer />
     </div>
