679093978184dab8164c390a845c975ce2d54f51
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-06-23 11:54:22 +0000

    Update 7 files

--- src/Pages/RandomQuotes.jsx
+++ src/Pages/RandomQuotes.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import RandomQuote from "../Components/RandomQuote";
+import { useNavigate } from "react-router-dom";
+import Button from "../Components/Button";
 
 const RandomQuotes = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="bg-blue-100 min-h-screen">
       <Header title={"Frases célebres"} />
+      <Button
+        type={"violet"}
+        className={"ml-8"}
+        onClickValue={() => {
+          navigate("/projects");
+        }}
+        value={"Proyectos"}
+      />
       <RandomQuote />
       <Footer />
     </div>
--- src/Pages/WeightTracker.jsx
+++ src/Pages/WeightTracker.jsx
@@ -1,11 +1,23 @@
 import WeightTable from "../Components/WeightTable";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
+import { useNavigate } from "react-router-dom";
+import Button from "../Components/Button";
 
 const WeightTracker = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="bg-blue-100 min-h-screen">
       <Header title={"Registro de peso"} />
+      <Button
+        type={"violet"}
+        className={"ml-8"}
+        onClickValue={() => {
+          navigate("/projects");
+        }}
+        value={"Proyectos"}
+      />
       <WeightTable />
       <Footer />
     </div>
